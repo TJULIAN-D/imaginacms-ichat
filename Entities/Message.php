feabef4c354fa535f0675f7da1d3707d5844cf0c
msolanogithub <msolano@imaginacolombia.com>
2024-07-05 16:24:46 -0500

    Fixed the load Media for Messages

--- Entities/Message.php
+++ Entities/Message.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Media\Support\Traits\MediaRelation;
 use Modules\Core\Support\Traits\AuditTrait;
 use Modules\Ichat\Entities\Status;
+use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
+use Illuminate\Support\Str;
+use Modules\Isite\Entities\Organization;
 
 class Message extends Model
 {
   protected $table = 'ichat__messages';
-  use MediaRelation, AuditTrait;
+  use MediaRelation, AuditTrait, belongsToTenant;
 
   protected $fillable = [
     'type',
@@ -45,7 +48,10 @@ class Message extends Model
     return $this->hasOne(Message::class, 'id', 'reply_to_id');
   }
 
-
+  public function organization()
+  {
+    return $this->belongsTo(Organization::class);
+  }
 
   /**
    * @return mixed
@@ -53,17 +59,28 @@ class Message extends Model
   public function getAttachmentAttribute()
   {
 
-    if(!empty($this->attached)){
+    if (!empty($this->attached)) {
       $thumbnail = $this->files()->where('zone', 'attachment')->first();
+
+      $tenancyMode = config("tenancy.mode", null);
+      $path = \URL::route('ichat.message.attachment', ["conversationId" => $this->conversation_id, "messageId" => $this->id, "attachmentId" => $this->attached]);
+
+      if (!empty($tenancyMode) && $tenancyMode == "singleDatabase" && !is_null($this->organization_id)) {
+        $path = tenant_route(
+          Str::remove('https://', $this->organization->url),
+          'ichat.message.attachment',
+          ["conversationId" => $this->conversation_id, "messageId" => $this->id, "attachmentId" => $this->attached]
+        );
+      }
+
       return [
         'mimetype' => $thumbnail->mimetype ?? '',
-        'path' => \URL::route('ichat.message.attachment', ["conversationId" => $this->conversation_id, "messageId" => $this->id, "attachmentId" => $this->attached]),
+        'path' => $path,
         'extension' => $thumbnail->extension ?? '',
         'filename' => $thumbnail->filename ?? '',
-        'filesize'=>$thumbnail->filesize ?? ''
+        'filesize' => $thumbnail->filesize ?? ''
       ];
-    }
-    else
+    } else
       return null;
 
   }
